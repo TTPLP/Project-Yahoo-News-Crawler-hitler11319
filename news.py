b4c4c73f3cc30f4c4fdc9aa23b2e9a00e83cfb1f
hitler11319 <hitler11319@gmail.com>
2015-12-26 11:22:38 +0800

    use lambda and add filter

--- news.py
+++ news.py
@@ -13,7 +13,7 @@ class News:
 
 
     def __str__(self):
-        return "topic:{topic} \n author:{author} \n date:{date} \n time:{time} \n text:{text} \n ".format(
+        return "topic:{topic} \n author:{author} \n date:{date} \n time:{time} \n text:{text} \n  \n".format(
             topic  =  self.topic,  
             author  =  self.author,  
             date  =  self.date,
@@ -32,22 +32,14 @@ class List_news():
     def len(self):
         return len(self.news)
 
+    def filter(self, func, output):
+        for item in self.news:
+            if func(item): output.write(str(item))
+
     def search_author(self, goal):
         output = open(goal + ".json", "wt")
-
-        count = 0
-
-        for y in self.news:
-            if y.author == goal:
-                output.write(str(y))
-                print("第", count, "則新聞已讀取")
-                count += 1
-
-        if count != 0:
-            print("讀取完畢")
-        else:
-            print("查無此資料")
-
+        
+        self.filter(lambda item: item.author == goal, output)
 
         output.close()
 
@@ -57,35 +49,13 @@ class List_news():
 
         output = open(first_goal_time + "o'clock to " + end_goal_time + "o'clcok news.json", "wt")
 
-        count = 0
-
-        for y in self.news:
-            if first_goal_time <= y.time.hour < end_goal_time:
-                output.wiite(str(y))
-                print("第", count, "則新聞已讀取")
-                count += 1
-
-        if count != 0:
-            print("讀取完畢")
-        else:
-            print("查無此資料")
+        self.filter(lambda item: first_goal_time <= item.time.hour < end_goal_time, output)
 
         output.close()
 
     def search_topic(self, keyword):
-        output = open("about", keyword, "news.json", "wt")
+        output = open("about" + keyword + "news.json", "wt")
 
-        count =0
-
-        for y in self.news:
-            if keyword in y.topic:
-                output.write(str(y))
-                print("第", count, "則新聞已讀取")
-                count += 1
-
-        if count != 0:
-            print("讀取完畢")
-        else:
-            print("查無此資料")
+        self.filter(lambda item: keyword in item.topic, output)
 
         output.close()
