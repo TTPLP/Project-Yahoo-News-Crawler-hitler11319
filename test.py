fd92e6e8b571c7ca8c80ed08f8a98bd7025f2fc0
hitler11319 <hitler11319@gmail.com>
2016-01-16 19:45:44 +0800

    coding style, remove file, open change with as

--- test.py
+++ test.py
@@ -1,35 +1,29 @@
+from datetime import datetime
 import unittest
+import os
+
 import news
 import web
-import datetime
+
 
 class News_mathod_test(unittest.TestCase):
     def setUp(self):
         self.news = news.List_news()
 
-        self.news.append(news.News('hello', 'me', datetime.date(2015, 10, 5), datetime.time(12, 50, 0), 'hello you are '))
-        self.news.append(news.News('ok', 'you', datetime.date(2015, 12, 18), datetime.time(18, 25, 0), 'are you ok '))
+        self.news.append(news.News('hello', 'me', datetime(2015, 10, 5, 12, 50, 0), 'hello you are '))
+        self.news.append(news.News('ok', 'you', datetime(2015, 12, 18, 18, 25, 0), 'are you ok' ))
 
     def test_append(self):
-        test_input = "this is test input"
+        test_input = news.News('a', 'a', datetime(1000, 6, 18, 1, 25, 0), 'a')
+
         self.assertEqual(len(self.news.append(test_input)), 3)
-
-    def test_save(self):
-        items = ['hello', 'ok']
-
-        output = open('t.json', "wt")
-        self.news.save(items, output)
-        output.close()
-
-        result = open('t.json', "rt").read()
-        self.assertEqual(result, 'hellook')
+        self.assertEqual(self.news[0], '123')
 
     def test_filter(self):
-        total = ""
         goal = "ok"
+        
         for i in self.news.filter(lambda item: goal in item.text):
-            total += i.topic
-        self.assertEqual(total, "ok")
+            self.assertIn("ok", i.topic)
 
     def test_change_time_format(self):
         date = "2015年12月20日 下午3:12"
@@ -42,41 +36,34 @@ class News_mathod_test(unittest.TestCase):
     def test_search_author(self):
         goal = 'me'
 
-        self.news.search_author(goal)
-
-        result = open('me.json', "rt").read()
-
-        self.assertEqual(len(result), 84)
+        for item in self.news.search_author(goal):
+            self.assertIn('me', item.author)
 
     def test_search_time(self):
         first_goal_time = 16
         end_goal_time = 20
 
-        self.news.search_time(first_goal_time, end_goal_time)
-
-        result = open('16  o\'clock to 20  o\'clcok news.json', "rt").read()
-
-        self.assertEqual(len(result), 79)
+        for item in self.news.search_time(first_goal_time, end_goal_time):
+            self.assertIn('ok', item.text)
 
     def test_search_topic(self):
         keyword = 'he'
 
-        self.news.search_topic(keyword)
-
-        result = open('about he news.json', "rt").read()
-
-        self.assertEqual(len(result), 84)
+        for item in self.news.search_topic(keyword):
+            self.assertIn('hello', item.topic)
 
     def test_againdeal(self):
+        base_url = 'https://tw.news.yahoo.com/'
         url_list = ['123']
-        output = open('t2.json', "wt")
 
-        web.againdeal(url_list, output)
+        with open('t.json', "wt", encoding = 'utf-8') as output:
+            web.againdeal(url_list, output, base_url)
+        
+        with open('t.json', "rt", encoding = 'utf-8') as result:
+            self.assertEqual(len(result.read()), 0)
 
-        output.close()
+        os.remove('t.json')
 
-        result = open('t2.json', "rt").read()
-        self.assertEqual(len(result), 0)
 
 if __name__ == '__main__':
     unittest.main()
