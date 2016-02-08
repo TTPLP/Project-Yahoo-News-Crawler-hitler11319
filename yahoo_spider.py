9920170fb94ef4b48257553c4860135bb1639af5
hitler11319 <hitler11319@gmail.com>
2016-02-08 19:13:12 +0800

    coding style

--- yahoo_spider.py
+++ yahoo_spider.py
@@ -2,5 +2,7 @@ import myspider
 
 if __name__ == '__main__':
     target = myspider.Spider('https://tw.news.yahoo.com/')
+    
     target.add_channel('https://tw.news.yahoo.com/society/')
+    
     target.start()
