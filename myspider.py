82820edfa80697331fdcb5d8d10e08fbba1d413c
hitler11319 <hitler11319@gmail.com>
2016-02-15 19:25:15 +0800

    3 port (spider, channel, page)
    Each channel Simultaneously crawl and save() certain threading is over just run

--- myspider.py
+++ myspider.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+import time
 import threading
 import requests
 import json
@@ -21,56 +22,95 @@ class News:
             text = self.text
         )
 
-    def toJson(self):
-        return json.dumps({'topic': self.topic, 'author': self.author, 'datetime': str(self.datetime), 'text': self.text}, ensure_ascii = False)
+    def toDict(self):
+        return {'topic': self.topic, 'author': self.author, 'datetime': str(self.datetime), 'text': self.text}
 
 
 
 class Spider:
     def __init__(self, first_url):
         self.first_url = first_url
-        self.channels = []
-        self.url_list = []
-        self.data = []
-
-    def add_channel(self, channel):
-        return self.channels.append(channel)
 
     def crawl(self, url):
         information = requests.get(url)
         information.encoding = 'utf-8'
         return information.text
 
-    def crawl_link(self, url):
-        data = self.crawl(url)
+    def save(self, data):
+        with open('items.json', 'wt', encoding = 'utf-8') as output:
+            output.write(json.dumps(data, ensure_ascii = False))
+
+    def start(self):
+        rlock = threading.RLock()
+
+        for channel in Channel().channels:
+            rlock.acquire()
+            thread = threading.Thread(target = Channel().crawl, args = (channel, self.first_url, ))
+            thread.start()
+            rlock.release()
+
+class Channel(Spider):
+    def __init__(self, channel = []):
+        self.channels = channel
+        
+    def add_channel(self, channel_list):
+        return self.channels.append(channel_list)
+
+    def crawl(self, url, first_url):
+        store = []    #use to store the channel each item url(already try: use threading , 3 store have own data, so not repeat)
+        
+        data = super().crawl(url)
         link = re.findall('<a href=\"/.*html\" class=\"title \"', data)
 
         # as Yahoo_news_crawler/crawler.py dealstr() same 
         for y in range(0, len(link)): 
-            link[y] = link[y].replace('html', ' + ', 100).replace('\"/', ' + ', 100).split(' + ')  
-
+            link[y] = link[y].replace('html', ' + ').replace('\"/', ' + ').split(' + ')  
 
         for item in link: 
             for it in range(0, len(item)):
                 if item[it][-1] == '.':
-                    self.url_list.append(item[it])
+                    Page().append_url(item[it])
+                    store.append(item[it])
 
-        return self.url_list
 
-    def crawl_data(self, url):
+
+        #every channel have own crawl url data
+        rlock = threading.RLock()
+        for items in store:
+            rlock.acquire()
+            thread1 = threading.Thread(target = Page().crawl, args = (items, first_url, ))
+            thread1.start()
+            rlock.release()
+            
+
+        return Page().url_list
+
+    def get_all(self):
+        return self.channels
+
+    def __len__(self):
+        return len(self.channels)
+
+
+class Page(Spider):
+    def __init__(self, url_list = [], data = []):
+        self.url_list = url_list
+        self.data = data
+        
+    def crawl(self, url, first_url):
         topic_split = re.compile('<h1 class=\"headline\">.*</h1>')
         author_split = re.compile('<span class=\"provider org\">.*</span>')
         date_split = re.compile('<abbr title=.*</abbr>')
         text_split = re.compile('<p class=\"first\">.*</p>|<p>.*</p>')
 
-        data = self.crawl(self.first_url + str(url) + 'html')
+        url_data = super().crawl(first_url + str(url) + 'html')
 
         try:
             #uer "str" ,  because list not use 
-            topic = str(topic_split.findall(data)).replace('<h1 class=\"headline\">', '').replace('</h1>', '').replace('\\u3000', '', 20).replace('╱', '', 10).replace('[', '', 10).replace(']', '',10)
-            author = str(author_split.findall(data)).replace('<span class=\"provider org\">', '').replace('</span>', '').replace('[', '', 10).replace(']', '',10)
-            date = str(date_split.findall(data)).replace('>', '<', 10).split('<')[2]      #this is so trouble,  it is ["",  "<abbr title = ...",  "date",  "</abbr>",  ""],  so is data[2]
-            text = str(text_split.findall(data)).replace('<p class=\"first\">', '').replace('</p>', '', 100).replace(' ', '', 100).replace('<p>', '', 100).replace('[', '', 10).replace(']', '',10).replace('\',\'', '', 10)
+            topic = str(topic_split.findall(url_data)).replace('<h1 class=\"headline\">', '').replace('</h1>', '').replace('\\u3000', '', 20).replace('╱', '', 10).replace('[', '', 10).replace(']', '',10)
+            author = str(author_split.findall(url_data)).replace('<span class=\"provider org\">', '').replace('</span>', '').replace('[', '', 10).replace(']', '',10)
+            date = str(date_split.findall(url_data)).replace('>', '<', 10).split('<')[2]      #this is so trouble,  it is ["",  "<abbr title = ...",  "date",  "</abbr>",  ""],  so is data[2]
+            text = str(text_split.findall(url_data)).replace('<p class=\"first\">', '').replace('</p>', '', 100).replace(' ', '', 100).replace('<p>', '', 100).replace('[', '', 10).replace(']', '',10).replace('\',\'', '', 10)
 
             #deal with date
             if '下' in date:
@@ -84,26 +124,23 @@ class Spider:
                 date = datetime.strptime(date, '%Y年%m月%d日 %H:%M') 
 
 
-            self.data.append(News(topic, author, date, text).toJson())
+            self.data.append(News(topic, author, date, text).toDict())
 
             print('第', len(self.data), '則新聞已擷取完')
+            
+            #certain threading is over
+            if len(self.url_list) == len(self.data):
+                print('資料擷取完畢')
+                super().save(self.data)
         except:
+            self.url_list.remove(url)   #let self.url_list long as same as self.data, so if not have data del it.
             raise UnicodeEncodeError
 
-    def save(self, data):
-        with open('items.json', 'wt', encoding = 'utf-8') as output:
-            output.write(json.dumps(data, ensure_ascii = False))
 
-    def start(self):
-        for channel in self.channels:
-            self.crawl_link(channel)
+    def __len__(self):
+        return len(self.data)
 
-        for url in self.url_list:
-            thread = threading.Thread(target = self.crawl_data, args = (url, ))
-            thread.start()
+    def append_url(self, obj):
+        return self.url_list.append(obj)
 
-        #until thread.start() end , run
-        thread.join()
-        self.save(self.data)
-
-            
+               
