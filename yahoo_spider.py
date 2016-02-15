5f1eb079c102f129ff2443f5f023681bf8ed7589
hitler11319 <hitler11319@gmail.com>
2016-02-15 19:22:18 +0800

    add more channel to test

--- yahoo_spider.py
+++ yahoo_spider.py
@@ -3,6 +3,10 @@ import myspider
 if __name__ == '__main__':
     target = myspider.Spider('https://tw.news.yahoo.com/')
     
-    target.add_channel('https://tw.news.yahoo.com/society/')
+    myspider.Channel().add_channel('https://tw.news.yahoo.com/society/')
+    myspider.Channel().add_channel('https://tw.news.yahoo.com/sports/')
+    myspider.Channel().add_channel('https://tw.news.yahoo.com/entertainment/')
     
     target.start()
+
+
